854573235cd0effcc000b6b87581ea80191d8471
Cameron MacFarland <distantcam@gmail.com>
2013-04-27 09:32:10 +0800

    Font change. New alignment of lines.

--- bargraph/bargraph.c
+++ bargraph/bargraph.c
@@ -6,7 +6,7 @@
 #define MY_UUID { 0x00, 0xB3, 0xC1, 0xF8, 0x31, 0xE6, 0x4E, 0x9A, 0x8E, 0x16, 0xFA, 0xB2, 0x18, 0xCF, 0x19, 0x83 }
 PBL_APP_INFO(MY_UUID,
              "Bar Graph", "Cameron MacFarland",
-             0, 9, /* App version */
+             1, 1, /* App version */
              RESOURCE_ID_IMAGE_MENU_ICON,
              APP_INFO_WATCH_FACE);
 
@@ -36,11 +36,11 @@ void update_hour(Layer *me, GContext* ctx) {
   graphics_context_set_fill_color(ctx, GColorWhite);
   graphics_context_set_stroke_color(ctx, GColorBlack);
 
-  graphics_fill_rect(ctx, GRect(38, 131 - height * 10, 30, height * 10), 0, GCornerNone);
+  graphics_fill_rect(ctx, GRect(38, 132 - height * 10, 30, height * 10), 0, GCornerNone);
 
   for (int i = 0; i < height-1; i++)
   {
-    graphics_draw_line(ctx, GPoint(38, 121 - i*10), GPoint(67, 121 - i*10));
+    graphics_draw_line(ctx, GPoint(38, 122 - i*10), GPoint(67, 122 - i*10));
   }
 }
 
@@ -55,11 +55,11 @@ void update_minute(Layer *me, GContext* ctx) {
   graphics_context_set_fill_color(ctx, GColorWhite);
   graphics_context_set_stroke_color(ctx, GColorBlack);
 
-  graphics_fill_rect(ctx, GRect(76, 131 - height, 30, height), 0, GCornerNone);
+  graphics_fill_rect(ctx, GRect(76, 132 - height, 30, height), 0, GCornerNone);
 
   for (int i = 10; i < height; i+=10)
   {
-    graphics_draw_line(ctx, GPoint(76, 131 - i), GPoint(105, 131 - i));
+    graphics_draw_line(ctx, GPoint(76, 132 - i), GPoint(105, 132 - i));
   }
 }
 
@@ -114,11 +114,11 @@ void handle_init(AppContextRef ctx) {
   minLayer.update_proc = &update_minute;
   layer_add_child(&window.layer, &minLayer);
 
-  text_layer_init(&dateText, GRect(0,135,144,30));
+  text_layer_init(&dateText, GRect(0,138,144,30));
   text_layer_set_text_color(&dateText, GColorWhite);
   text_layer_set_background_color(&dateText, GColorClear);
   text_layer_set_text_alignment(&dateText, GTextAlignmentCenter);
-  text_layer_set_font(&dateText, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_22)));
+  text_layer_set_font(&dateText, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ARIAL_22)));
   layer_add_child(&window.layer, &dateText.layer);
 
   update_date();
